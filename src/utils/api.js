8835b732da7ba4e08f1eba5f7a6aa22e35b0810a
Khanyisa Mhlarhi <khanyisamhlarhi@gmail.com>
2024-02-16 14:10:51 +0200

    hardcoded api credentials

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,5 @@
-const API_KEY = process.env.REACT_APP_API_KEY;
-const APP_ID = process.env.REACT_APP_ID;
+// const API_KEY = process.env.REACT_APP_API_KEY;
+// const APP_ID = process.env.REACT_APP_ID;
 
 export const API_URL = 'https://api.edamam.com/api/recipes/v2';
 
@@ -7,8 +7,8 @@ export const fetchRecipes = async (query, listOfIngredients) => {
 	const params = new URLSearchParams({
 		type: 'public',
 		q: [query,...listOfIngredients].join(' '),
-		app_id: process.env.REACT_APP_ID,
-		app_key: API_KEY,
+		app_id: '665013aa',
+		app_key: '47fda470e7388732b8fce6e5bfb94b19',
 	});
 	
 	const url = `${API_URL}?${params}`;
